4fb37b08e38f2e4047701735e7b050cc5b5d724c
Freewind <nowindlee@gmail.com>
2020-04-14 14:54:47 +0800

    improve code

--- hello.ts
+++ hello.ts
@@ -1,16 +1,16 @@
-import * as Koa from 'koa';
-import * as Router from 'koa-router';
+import Koa from 'koa';
+import Router from 'koa-router';
 
 const app = new Koa();
 
 const router = new Router();
 router.get('/hello', ctx => {
-    ctx.body = 'Hello!'
+  ctx.body = 'Hello!'
 });
 
 app.use(router.routes());
 
 app.listen(3000, () => {
-    console.log('http://localhost:3000');
+  console.log('http://localhost:3000');
 });
 
